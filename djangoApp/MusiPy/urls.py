62c7b6fa5b2849e408bdc85843b1b8181f69b690
alfhad <fahadshah2411@gmail.com>
2021-03-26 19:01:38 +0530

    done

---
+++ djangoApp/MusiPy/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('song/', SongApiView.as_view()),
+    path('song/<int:id>/', SongDetails.as_view()),
+    path('podcast/', PodcastApiView.as_view()),
+    path('podcast/<int:id>/', PodcastDetails.as_view()),
+    path('audiobook/', AudioBookApiView.as_view()),
+    path('audiobook/<int:id>/', AudioBookDetails.as_view()),
+    path('addfile/', AddAFile.as_view())
+]
